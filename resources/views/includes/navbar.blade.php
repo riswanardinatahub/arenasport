c611058b36e9615ac228b2a58a4ae546ca39f609
qlokgit <Qlokgit@gmail.com>
2023-02-26 22:46:27 +0700

    Update

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -103,7 +103,7 @@
 
           @if (Auth::user()->store_name)
             <div class="dropdown-menu">
-            <a href="{{ route('dashboard') }}" class="dropdown-item">Dashboard AYANG</a>
+            <a href="{{ route('dashboard') }}" class="dropdown-item">Dashboard</a>
             <a href="{{ route('dashboard-settings-account') }}" class="dropdown-item">Settings</a>
             <div class="dropdown-divider"></div>
             <a href="{{ route('logout') }}" onclick="event.preventDefault();
